b90c9fe770e8c7257194cdffe4898cee70535178
ghriim <vignierquentin@yahoo.fr>
2018-11-21 16:42:34 +0100

    Navigation from login to home

---
+++ app/src/main/java/com/cuteninja/shf/kotlin/shfkotlin/workout/WorkoutListActivity.kt
@@ -0,0 +1,13 @@
+package com.cuteninja.shf.kotlin.shfkotlin.workout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cuteninja.shf.kotlin.shfkotlin.R
+
+class WorkoutListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_list)
+    }
+}
