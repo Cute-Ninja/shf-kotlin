5bde7a1b2dd5b5fd195657ffd65bea57ad48458d
ghriim <vignierquentin@yahoo.fr>
2018-11-23 16:24:42 +0100

    Set up shared menu and navigation

--- app/src/main/java/com/cuteninja/shf/kotlin/shfkotlin/HomeActivity.kt
+++ app/src/main/java/com/cuteninja/shf/kotlin/shfkotlin/HomeActivity.kt
@@ -1,14 +1,28 @@
 package com.cuteninja.shf.kotlin.shfkotlin
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.FragmentManager
 import com.cuteninja.shf.kotlin.shfkotlin.common.AbstractDrawerActivity
 
 class HomeActivity : AbstractDrawerActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        super.addActionBar()
+        intView()
+    }
+
+    private fun intView() {
+        // @Todo replace by test about user connected
+        if (true) {
+            super.initDrawerAndToolbar()
+
+            val frameLayout = findViewById<FrameLayout>(R.id.layout_content)
+            layoutInflater.inflate(R.layout.activity_home, frameLayout, true)
+        } else {
+            // @Todo redirect to log screen
+        }
     }
 }
--- app/src/main/java/com/cuteninja/shf/kotlin/shfkotlin/common/AbstractDrawerActivity.kt
+++ app/src/main/java/com/cuteninja/shf/kotlin/shfkotlin/common/AbstractDrawerActivity.kt
@@ -6,17 +6,32 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.cuteninja.shf.kotlin.shfkotlin.R
-import kotlinx.android.synthetic.main.activity_home.*
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_drawer.*
+
 
 abstract class AbstractDrawerActivity : AppCompatActivity() {
 
-    protected fun addActionBar() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+    }
+
+    /**
+     * Initialize the navigation drawer and the support in the UI
+     */
+    protected fun initDrawerAndToolbar() {
+        initToolbar()
+    }
+
+
+    private fun initToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val drawerToggle = object : ActionBarDrawerToggle(
             this,
-            drawerLayout,
+            drawer_layout,
             toolbar,
             R.string.drawer_open,
             R.string.drawer_close
@@ -33,11 +48,11 @@ abstract class AbstractDrawerActivity : AppCompatActivity() {
         }
 
         drawerToggle.isDrawerIndicatorEnabled = true
-        drawerLayout.addDrawerListener(drawerToggle)
+        drawer_layout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
 
-        navigationView.setNavigationItemSelectedListener {
-            drawerLayout.closeDrawer(GravityCompat.START)
+        nav_view.setNavigationItemSelectedListener {
+            drawer_layout.closeDrawer(GravityCompat.START)
             true
         }
     }
@@ -45,7 +60,7 @@ abstract class AbstractDrawerActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             android.R.id.home -> {
-                drawerLayout.openDrawer(GravityCompat.START)
+                drawer_layout.openDrawer(GravityCompat.START)
                 true
             }
             else -> super.onOptionsItemSelected(item)
